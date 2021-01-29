0766b07de84ebf75caa170eb1b25edd875583037
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2021-01-29 20:07:38 +0200

    basic fuel database

--- src/main/java/fuels/FuelDatabase.java
+++ src/main/java/fuels/FuelDatabase.java
@@ -1,7 +1,34 @@
 package fuels;
 
+import java.util.Map;
+
 public class FuelDatabase {
 
+    public static Map<String, FuelParameters> fuelMap;
+
+    public static class FuelParameters {
+        public double energyPerMass;
+        public double density;
+
+        FuelParameters(double _energyPerMass, double _density){
+            energyPerMass = _energyPerMass; // J/kg
+            density = _density; // kg/m3
+        }
+    }
+
+    public static void addNewFuel(String name, FuelParameters parameters){
+        if (checkFuelDefined(name)){
+            System.err.println("Trying to add already defined fuel to the fuel database.");
+        } else {
+            fuelMap.put(name, parameters);
+        }
+    }
+
+    private static boolean checkFuelDefined(String name){
+        return !fuelMap.isEmpty() &&
+                fuelMap.containsKey(name) &&
+                fuelMap.get(name) != null;
+    }
 
 
 }
