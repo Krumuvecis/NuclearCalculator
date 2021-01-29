ec6f4c667b1c839abc2292a6465d0336be4dd77f
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2021-01-29 20:33:00 +0200

    improved case analysis

--- src/main/java/fuels/FuelDatabase.java
+++ src/main/java/fuels/FuelDatabase.java
@@ -10,24 +10,28 @@ public class FuelDatabase {
         public double energyPerMass;
         public double density;
 
-        FuelParameters(double _energyPerMass, double _density){
+        FuelParameters(
+                double _energyPerMass,
+                double _density){
             energyPerMass = _energyPerMass; // J/kg
             density = _density; // kg/m3
         }
     }
 
     public static void addNewFuel(String name, FuelParameters parameters){
-        if (checkFuelDefined(name)){
-            System.err.println("Trying to add already defined fuel to the fuel database.");
+        if (name == null || name.equals("")){
+            System.err.println("Trying to add an unnamed fuel to the fuel database.");
+        } else if (checkFuelDefined(name)){
+            System.err.println("Trying to add an already defined fuel to the fuel database.");
         } else {
             fuelMap.put(name, parameters);
         }
     }
 
     private static boolean checkFuelDefined(String name){
-        return !fuelMap.isEmpty() &&
+        return (!fuelMap.isEmpty() &&
                 fuelMap.containsKey(name) &&
-                fuelMap.get(name) != null;
+                fuelMap.get(name) != null);
     }
 
 
