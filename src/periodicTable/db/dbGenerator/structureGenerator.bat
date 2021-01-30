92d1b151250ef869f6842ce9d9c65e6c252f3fe2
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2021-01-30 20:24:55 +0200

    database generator improvements

--- src/periodicTable/db/dbGenerator/structureGenerator.bat
+++ src/periodicTable/db/dbGenerator/structureGenerator.bat
@@ -1,13 +1,45 @@
+@echo off
 set plus=+
 set one=1
-
-set element=1
-set maxelement=5
-
 cd ..\
 
+:start
+cls
+echo Welcome to Periodic Table generator!
+echo.
+echo Operational modes:
+echo 1 - first time setup
+echo 2 - edit values
+echo x - exit
+echo.
+set modeChoice=
+set /p modeChoice=Select operational mode: 
+if %modeChoice% == 1 goto firstSetup
+if %modeChoice% == 2 goto valueEdit
+if %modeChoice% == x exit
+goto start
+
+:valueEdit
+echo.
+echo This functionality is not working yet.
+echo.
+pause
+goto start
+
+:firstSetup
+cls
+echo First time database setup.
+echo Type x to go back.
+echo.
+set element=1
+set maxelement=0
+set /p maxelement=Choose the number of elements: 
+if %maxelement% == x goto start
+if %maxelement% GEQ %element% goto generateElement
+goto firstSetup
+
 :generateElement
-echo generating element %element%
+echo generating element [%element%/%maxelement%]
 mkdir %element%
 cd %element%
 goto generateElementalData
@@ -19,7 +51,7 @@ goto compareElement
 
 :compareElement
 cd ..\
-if %element% == %maxelement% goto end
+if %element% GEQ %maxelement% goto end
 set /a element=%element%%plus%%one%
 goto generateElement
 
