f599886890f70bf9623edec247c44db56261943e
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2021-01-31 03:55:16 +0200

    0.3
    * General restructure
    
    * Database restructure
    
    * Database manager added
    
    * Database documentation added

---
+++ src/periodicTable/db/dbManager/Manager.bat
@@ -0,0 +1,41 @@
+@echo off
+set titleName=Periodic Table Manager
+title %titleName%
+set managerDir=dbManager
+cd ..\
+
+:start
+set setupStatus= 
+if exist %managerDir%\setupStatus set /p setupStatus=<%managerDir%\setupStatus
+set setupStatusOutput=Database status unknown
+if %setupStatus% == true set setupStatusOutput=Database already set up
+if %setupStatus% == false set setupStatusOutput=Database setup needed
+
+cls
+echo Welcome to %titleName%!
+echo.
+echo Operational modes:
+echo 1 - first time setup (%setupStatusOutput%)
+echo 2 - edit values
+echo x - exit
+echo.
+set modeChoice= 
+set /p modeChoice=Select operational mode: 
+if %modeChoice% == 1 goto firstSetup
+if %modeChoice% == 2 goto valueEdit
+if %modeChoice% == x exit
+goto start
+
+:firstSetup
+echo.
+echo First time setup running...
+start /w %managerDir%\setup.bat %managerDir%
+goto start
+
+:valueEdit
+echo.
+echo Warning: This functionality is not implemented yet.
+echo Currently changes can only be made manually.
+echo.
+pause
+goto start
---
+++ src/periodicTable/db/dbManager/elementalDataGenerator.bat
@@ -0,0 +1,6 @@
+@echo off
+title Elemental data generator
+cls
+echo symbol:%1>main
+echo name:%2>>main
+exit
